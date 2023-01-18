e1390300a5f9aed1166393580c2dbd5cc19a7e11
harshal-kitukale <harshalkitukale005@gmail.com>
2023-01-18 20:35:07 +0530

    homepage added

--- apnamall/next.config.js
+++ apnamall/next.config.js
@@ -1,6 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+	// reactStrictMode: true,
+	swcMinify: true,
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "rukminim1.flixcart.com",
+				port: "",
+				pathname: "/image/612/612/**",
+			},
+		],
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "assetscdn1.paytm.com",
+				port: "",
+				pathname: "/images/catalog/view_item/**",
+			},
+		],
+	},
+   
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- apnamall/src/pages/_app.js
+++ apnamall/src/pages/_app.js
@@ -1,10 +1,25 @@
-import '@/styles/globals.css'
-import { ChakraProvider } from '@chakra-ui/react'
-
+import "@/styles/globals.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import "../styles/featuredProducts.css";
 export default function App({ Component, pageProps }) {
-  return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
-  );
+	return (
+		<>
+			<link
+				href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+				rel="stylesheet"
+			/>
+
+			<link
+				rel="stylesheet"
+				href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.1.1/css/all.css"
+			/>
+			{/* <script
+				src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+				async></script> */}
+
+			<ChakraProvider>
+				<Component {...pageProps} />
+			</ChakraProvider>
+		</>
+	);
 }
