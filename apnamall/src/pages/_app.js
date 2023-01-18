395cdaf812130883835712e11706a8f464c88ea2
itsvickyanand <vickyanandtoprock@gmail.com>
2023-01-18 19:21:35 +0530

    navbar and footer completed

--- apnamall/src/pages/_app.js
+++ apnamall/src/pages/_app.js
@@ -1,10 +1,14 @@
+import Footer from '@/components/Footer'
+import Navbar from '@/components/Navbar'
 import '@/styles/globals.css'
 import { ChakraProvider } from '@chakra-ui/react'
 
 export default function App({ Component, pageProps }) {
   return (
     <ChakraProvider>
+      <Navbar />
       <Component {...pageProps} />
+      <Footer />
     </ChakraProvider>
-  );
+  )
 }
