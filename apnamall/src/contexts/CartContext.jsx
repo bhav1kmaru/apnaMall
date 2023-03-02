2b600977ee54214d3313efb40f0e84088b01ae02
bhav1kmaru <bhavik.m01@gmail.com>
2023-03-03 00:28:42 +0530

    some final changes for day-1

--- apnamall/src/contexts/CartContext.jsx
+++ apnamall/src/contexts/CartContext.jsx
@@ -6,10 +6,10 @@ export const CartContext=createContext()
 export default function CartContextProvider({children}){
     const [cartLength,setCartLength]=useState(0);
 
-    // useEffect(()=>{
-    //     const cart=JSON.parse(localStorage.getItem('cart'))
-    //     setCartLength(cart.length)
-    // },[])
+    useEffect(()=>{
+        const cart=JSON.parse(localStorage.getItem('cart'))
+        setCartLength(cart.length)
+    },[])
 
     return <CartContext.Provider value={{cartLength,setCartLength}}>{children}</CartContext.Provider>
 }
